00b8df8d4fe73a619bb3fa5793b5ae13150c53f4
Bladesheng <keadr23@gmail.com>
2022-05-06 19:22:55 +0200

    Add function for creating the empty grid itself

--- javascript.js
+++ javascript.js
@@ -1,9 +1,33 @@
-// creates x lines for the grid
+// initial setup
+makeGrid(16);
 
+// creates x lines for the grid
+function makeLines(lines) {
+  const playground = document.querySelector(".playground");
+  for (let i = 1; i <= lines; i++) {
+    const div = document.createElement("div");
+    div.classList.add("line");
+    playground.appendChild(div);
+  }
+}
 
 // creates x boxes in one line
+function makeSquares(line, squares) {
+  for (let i = 1; i <= squares; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    line.appendChild(div);
+  }
+}
 
 // creates the grid for drawing
+function makeGrid(dimension) {
+  makeLines(dimension);
+  const lines = document.querySelectorAll(".line");
+  for (const line of lines) {
+    makeSquares(line, dimension);
+  }
+}
 
 // hooks up the event listeners
 
